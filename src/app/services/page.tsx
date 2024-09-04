36265ade6774223eed04331130bd871a4d8990cb
seba espinosa <sebaespinosa@gmail.com>
2024-09-04 10:58:57 -0400

    services listo

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,54 @@
+
+'use client'
+import {motion} from 'framer-motion';
+
+import { fadeIn } from "../../../variants";
+import Transition from "@/components/Transition";
+import ServiceSlider from "@/components/ServiceSlider";
+import Bulb from "@/components/Bulb";
+import Circles from "@/components/Circles";
+
 export default function Services() {
     return(
-        <div>Services</div>
+        <>
+        <Transition />
+        <div className="h-screen bg-primary/30 py-36 flex items-center">
+            <Circles />
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row gap-x-8">
+                    {/* text */}
+                    <div className='text-center flex xl:w-[350px] flex-col lg:text-left mb-4 xl:mb-0'>
+                        <motion.h2 
+                         variants={fadeIn('up', 0.3)}
+                         initial='hidden'
+                         animate='show'
+                         exit='hidden'
+                         className="h2 xl:mt-8">
+                            My services <span className='text-accent'>.</span>
+                        </motion.h2>
+                        <motion.p 
+                         variants={fadeIn('up', 0.4)}
+                         initial='hidden'
+                         animate='show'
+                         exit='hidden'
+                         className='mb-4 max-w-[350px] mx-auto lg:mx-0'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. A distinctio optio consequuntur perferendis doloremque, provident eligendi hic ipsa nesciunt sed.
+                        </motion.p>
+                    </div>
+
+                    <motion.div
+                     variants={fadeIn('down', 0.6)}
+                     initial='hidden'
+                     animate='show'
+                     exit='hidden'
+                     className="w-full xl:max-w-[65%]">
+                        {/* slider */}
+                        <ServiceSlider />
+                    </motion.div>
+                </div>
+            </div>
+            <Bulb />
+        </div>
+        </>
     )
 }
--- src/components/Bulb.tsx
+++ src/components/Bulb.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image"
+
 const Bulb = () => {
   return (
-    <div>Bulb</div>
+    <div className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px]">
+      <Image
+        width={260}
+        height={200}
+        src={'/bulb.png'}
+        alt=""
+        className="w-full h-full"
+      />
+    </div>
   )
 }
 
